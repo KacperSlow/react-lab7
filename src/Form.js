9acc2d1c4bd870c88ffb450667df55e95b26f2d7
KacperSlow <k.slowikowski@student.mini.pw.edu.pl>
2019-11-23 17:06:13 +0100

    Lab7Ex3

--- src/Form.js
+++ src/Form.js
@@ -3,22 +3,56 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            age: 0
+            age: 0,
+            PhoneNumber: "" ,
+            email: "",
+            Valid: true
         }
       }
 
-    handleChangeName =(e) =>{
-        this.setState({age: e.target.value})
+    handleChangeAge =(e) =>{
+        this.setState({age: e.target.value, Valid: true, email: "",  PhoneNumber: ""})
     }
-    render(){  
+    handlePhoneNumber = (e) =>{
+        this.setState({PhoneNumber: e.target.value, Valid: true})
+    }
+    handleEmail = (e) =>{
+        this.setState({email: e.target.value, Valid: true})
+    }
+    handleValid = (e) => {
+        e.preventDefault();
+        let Pattern = /^\d{9}$/
+        if(this.state.age > 18)
+            Pattern = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+        Pattern.test(e.target.Validation.value) ?
+        this.setState({Valid: true})
+        :
+        this.setState({Valid: false})
+    }
+    render(){      
             
     return(
-        <div style={{border:'2px solid black', padding:'3px', marginLeft: '35%', marginRight: '40%', marginTop: '10%'}}>
-                <form>
-                    <p>Age: <input onChange={this.handleChangeName} type="number" name="age"></input></p>
-                    {this.state.age < 18 ? <p>Parent name: <input></input></p> : <p>Name: <input></input></p>}                 
-                    {this.state.age < 18 ? <p>Parent Number: <input></input></p> : <p>Email: <input></input></p>}          
-                    <button style={{position: 'absolute', right: '45%'}} type="button">Submit</button>
+        <div style={{border:'2px solid black', padding:'3px', marginLeft: '35%', marginRight: '40%', marginTop: '10%', position: 'relative'}}>
+                <form onSubmit={this.handleValid}>
+                    <p>Age:</p> <input onChange={this.handleChangeAge} type="number" name="age"></input>
+                    {this.state.age < 18 ? 
+                    <div><p>Parent name: </p> <input></input></div>
+                    :
+                    <div><p>Name:</p> <input></input></div>}                 
+                    {this.state.age < 18 ? 
+                    <div>
+                        <p>Parent Number:</p>
+                        <input onChange={this.handlePhoneNumber} name="Validation" value={this.state.PhoneNumber}></input>
+                        {this.state.Valid ? null : <p><label> Incorect Phone number</label></p>}
+                    </div>
+                    :
+                    <div>
+                        <p>Email:</p>
+                        <input name="Validation" onChange={this.handleEmail} value={this.state.email}></input>
+                        {this.state.Valid ? null : <p><label> Incorect email </label></p>}
+                    </div>}
+                    <br />          
+                    <button style={{position: 'absolute', right: '45%'}}>Submit</button>
                 </form>
             <br />
         </div>
